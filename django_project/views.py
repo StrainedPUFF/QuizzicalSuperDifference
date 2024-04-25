350ed8e7588897822b452d5324da4055be2f6886
StrainedPUFF <168001964+StrainedPUFF@users.noreply.github.com>
2024-04-25 06:54:28 +0000

    ADDED CONTENT

---
+++ django_project/views.py
@@ -0,0 +1,18 @@
+import requests
+from django.shortcuts import render
+
+
+def index(request):
+    r1 = requests.get('https://api.github.com/events')
+    data = r1.json()
+    events = data[0]['id']
+
+    r2 = requests.get('https://www.boredapi.com/api/activity')
+    data = r2.json()
+    activity = data['activity']
+
+    r3 = requests.get('https://dog.ceo/api/breeds/image/random')
+    res3 = r3.json()
+    dog =res3['message']
+  
+    return render(request, 'templates/index.html', {'events': events, 'activity': activity, 'dog': dog})
